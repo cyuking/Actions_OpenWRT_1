539d5d4ad8ffb0563996662151ed59fef3abdfd0
cyuking <87430111+cyuking@users.noreply.github.com>
2022-05-21 16:19:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,13 +9,13 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-git clone https://github.com/AlexZhuo/luci-app-bandwidthd package/luci-app-bandwidthd
+git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/luci-app-bandwidthd
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 # 删除自定义源默认的 argon 主题
-rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
 
 # 拉取 argon 原作者的源码
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 替换默认主题为 luci-theme-argon
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
